9f41c4349c34c9cd6e1c36a48662538ce9b2947a
Freivin Campbell <freivin12@gmail.com>
2021-04-25 21:32:43 -0600

    Using new dependencies on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
 	imports: [
@@ -16,7 +17,8 @@ import { CoffeesModule } from './coffees/coffees.module';
 			autoLoadEntities: true,
 			synchronize: true
 		}),
-		CoffeesModule
+		CoffeesModule,
+		CoffeeRatingModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
