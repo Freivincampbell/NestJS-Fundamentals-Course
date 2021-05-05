41705ff6adf5e101c492e3071bdaf231757d6379
Freivin Campbell <freivin12@gmail.com>
2021-05-04 23:09:43 -0600

    new global  filters and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -14,6 +18,14 @@ async function bootstrap() {
 			}
 		})
 	);
+
+	app.useGlobalInterceptors(
+		new WrapResponseInterceptor(),
+		new TimeoutInterceptor()
+	);
+
+	app.useGlobalFilters(new HttpExceptionFilter());
+
 	await app.listen(3000);
 }
 bootstrap().catch(() => 'Error');
